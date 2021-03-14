adc395f039c6b8b125609662f1d52ae5e31f1deb
Damien McDonnell <dm@cct-marketing.com>
2021-03-14 08:12:59 +0000

    Bug fix on textareas in ContactForm

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -5,14 +5,8 @@
     class="page-content-wrapper"
     :class="panelContents.form_wrapper_class"
   >
-    <div
-      class="page-content"
-      :class="panelContents.form_class"
-    >
-      <p
-        v-if="panelContents.intro_text"
-        class="mb-4"
-      >
+    <div class="page-content" :class="panelContents.form_class">
+      <p v-if="panelContents.intro_text" class="mb-4">
         {{ panelContents.intro_text }}
       </p>
 
@@ -21,8 +15,8 @@
         :name="panelContents.name"
         method="post"
         data-netlify-honeypot="bot-field"
-        @submit.prevent="handleSubmit"
         class="w-full grid grid-cols-1 gap-6"
+        @submit.prevent="handleSubmit"
       >
         <input type="hidden" name="form-name" :value="panelContents.name" />
 
@@ -30,17 +24,28 @@
           v-for="form_field in panelContents.form_fields"
           :key="form_field.name"
           class="w-full mb-2"
+          :class="{ 'pb-8': form_field.type == 'textarea' }"
         >
-          <label class="block">
+          <label
+            class="block"
+            :class="{ 'h-full': form_field.type == 'textarea' }"
+          >
             <span>{{ form_field.label }}</span>
-            <input :type="form_field.type" class="mt-1 block w-full" :rows="{ '3': panelContents.type == 'textarea' }">
+            <input
+              :type="form_field.type"
+              class="mt-1 block w-full"
+              :class="{ 'h-full': form_field.type == 'textarea' }"
+              :rows="form_field.type == 'textarea' ? 4 : null"
+            />
           </label>
         </div>
 
         <button
           type="submit"
           class="px-8 py-4 bg-gray-900 hover:bg-gray-700 text-white uppercase rounded"
-        >{{ panelContents.submit_button_text }}</button>
+        >
+          {{ panelContents.submit_button_text }}
+        </button>
       </form>
     </div>
   </div>
@@ -51,8 +56,8 @@ export default {
   name: 'ContactForm',
 
   props: {
-    panelContents: Object
-  }
+    panelContents: Object,
+  },
 }
 </script>
 
