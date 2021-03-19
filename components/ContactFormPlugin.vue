9ef14e6d341d60def8ea16e83851e3119f0198e4
Damien McDonnell <dm@cct-marketing.com>
2021-03-19 17:11:41 +0000

    Removed encode function temporarily

--- components/ContactFormPlugin.vue
+++ components/ContactFormPlugin.vue
@@ -161,13 +161,13 @@ export default {
       console.log({ body })
     },
 
-    encode(data) {
-      return Object.keys(data)
-        .map(
-          (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
-        )
-        .join('&')
-    },
+    // encode(data) {
+    //   return Object.keys(data)
+    //     .map(
+    //       (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+    //     )
+    //     .join('&')
+    // },
   },
 }
 </script>
