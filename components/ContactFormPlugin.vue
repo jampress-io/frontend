4ab0a773cd4bb04f66a47f612c9d0dc1a5c12b92
Damien McDonnell <dm@cct-marketing.com>
2021-03-19 16:49:42 +0000

    Contact Form 7 sending data to WP

--- components/ContactFormPlugin.vue
+++ components/ContactFormPlugin.vue
@@ -9,14 +9,66 @@
         {{ panelContents.intro_text }}
       </p>
 
-      <div :id="panelContents.form_id" :ref="panelContents.form_id" class="">
-        <p>Form will be displayed here...</p>
+      <div
+        :id="panelContents.form_id"
+        :ref="panelContents.form_id"
+        class="w-full"
+      >
+        <form
+          v-if="form.id"
+          :id="form.id"
+          :name="form.id"
+          @submit.prevent="handleSubmit"
+        >
+          <div
+            v-for="formField in form.fields"
+            :key="formField.name"
+            class="w-full mb-2"
+            :class="{ 'pb-8': formField.type == 'textarea' }"
+          >
+            <input
+              v-if="formField.type === 'hidden'"
+              :type="formField.type"
+              :name="formField.name"
+              :value="formField.value"
+            />
+
+            <label
+              v-else-if="formField.type !== 'submit'"
+              class="block"
+              :class="{ 'h-full': formField.type == 'textarea' }"
+            >
+              <span>{{ formField.label }}</span>
+              <input
+                v-if="formField.type !== 'textarea'"
+                :type="formField.type"
+                :name="formField.name"
+                class="mt-1 block w-full text-black"
+              />
+              <textarea
+                v-if="formField.type == 'textarea'"
+                :name="formField.name"
+                class="mt-1 block w-full text-black"
+                rows="4"
+              ></textarea>
+            </label>
+          </div>
+
+          <button
+            type="submit"
+            class="px-8 py-4 bg-gray-900 hover:bg-gray-700 text-white uppercase rounded"
+          >
+            {{ form.submitValue }}
+          </button>
+        </form>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'ContactForm',
 
@@ -30,11 +82,16 @@ export default {
   },
 
   data() {
-    return {}
+    return {
+      form: {
+        id: null,
+        fields: [],
+        submitValue: '',
+      },
+    }
   },
 
   mounted() {
-    const formContents = []
     const formFragment = document
       .createRange()
       .createContextualFragment(this.panelContents.form_shortcode)
@@ -43,14 +100,65 @@ export default {
 
     const contactFormFields = [...contactForm]
     contactFormFields.forEach((contactFormField) => {
-      formContents.push(contactFormField)
+      // console.warn('Field')
+      // console.log(contactFormField)
+
+      const formField = {}
+      const fieldLabel = contactFormField.closest('label')
+
+      if (contactFormField.name === '_wpcf7') {
+        this.form.id = contactFormField.value
+      }
+
+      if (contactFormField.type === 'submit') {
+        this.form.submitValue = contactFormField.value
+      }
+
+      if (contactFormField.type !== 'submit') {
+        formField.type = contactFormField.type
+        formField.name = contactFormField.name
+        formField.value = contactFormField.value
+        formField.required = contactFormField['aria-required']
+        if (fieldLabel) {
+          formField.label = fieldLabel.textContent
+        }
+
+        this.form.fields.push(formField)
+      }
     })
   },
 
   methods: {
-    handleSubmit() {
+    async handleSubmit() {
       // event.preventDefault()
       console.warn('Contact Form Submitted')
+
+      const submittedContactForm = document.getElementById(this.form.id)
+      // console.log({ submittedContactForm })
+      const formData = new FormData(submittedContactForm)
+      console.log({ formData })
+
+      const formSubmitUrl = `${process.env.baseUrl}/wp-json/contact-form-7/v1/contact-forms/${this.form.id}/feedback`
+      console.log({ formSubmitUrl })
+      // const url = `${process.env.baseUrl}/wp-json/contact-form-7/v1/contact-forms/${this.form.id}/feedback`
+      // console.log({ url })
+
+      // const formResponse = await axios.post({
+      //   method: 'POST',
+      //   url: formSubmitUrl,
+      //   data: formData,
+      //   headers: { 'Content-Type': 'application/json' },
+      // })
+      // const body = await formResponse.json()
+      // console.log({ body })
+
+      const res = await fetch(formSubmitUrl, {
+        method: 'POST',
+        body: formData,
+        mode: 'no-cors',
+      })
+      const body = await res
+      console.log({ body })
     },
 
     encode(data) {
