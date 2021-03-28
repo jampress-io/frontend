9b207f26aea56171632a29ba895221450abddd94
Damien McDonnell <dm@cct-marketing.com>
2021-03-28 16:21:07 +0100

    Full static generation activated

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ const dynamicRoutes = async () => {
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
