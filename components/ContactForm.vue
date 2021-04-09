00d18045999044da444967f32423bce683f36d3b
Damien McDonnell <dm@cct-marketing.com>
2021-04-10 00:04:05 +0100

    Updated ContactForm submit button colours

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -49,7 +49,7 @@
 
         <button
           type="submit"
-          class="px-8 py-4 bg-gray-900 hover:bg-gray-800 transition duration-200 ease-out text-white uppercase rounded"
+          class="px-8 py-4 bg-pink-700 hover:bg-indigo-600 transition duration-500 ease-out text-gray-900 uppercase rounded font-bold"
         >
           {{ panelContents.submit_button_text }}
         </button>
